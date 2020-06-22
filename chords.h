eb562363aeef2abe81101e9b8463ea7aef8ef517
FJB-ouweDremelaar <ferrie.bank@gmail.com>
2020-06-22 20:00:42 +0200

    first commit

---
+++ chords.h
@@ -0,0 +1,9 @@
+const int chord_length = 6;
+
+const uint16_t CHORD_A5[] = {NOTE_A5, NOTE_CS4, NOTE_E4, NOTE_A5, NOTE_CS4, NOTE_E4};
+const uint16_t CHORD_C5[] = {NOTE_C5, NOTE_E4, NOTE_G4, NOTE_C5, NOTE_E4, NOTE_G4};
+const uint16_t CHORD_D5[] = {NOTE_D5, NOTE_FS4, NOTE_A4, NOTE_D5, NOTE_FS4, NOTE_A4};
+const uint16_t CHORD_F5[] = {NOTE_F5, NOTE_A4, NOTE_C4, NOTE_F5, NOTE_A4, NOTE_C4};
+const uint16_t CHORD_GS5[] = {NOTE_GS5, NOTE_C4, NOTE_DS4, NOTE_GS5, NOTE_C4, NOTE_DS4};
+const uint16_t CHORD_G5[] = {NOTE_G5, NOTE_B4, NOTE_D4, NOTE_G5, NOTE_B4, NOTE_D4};
+const uint16_t CHORD_REST[] = {0, 0, 0, 0, 0, 0};
